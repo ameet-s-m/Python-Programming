9edcabb316c9bcd7203d335bd674eccfe8151c62
Ameet Shankargouda Munavalli <eng23ds0002@dsu.edu.in>
2024-10-12 13:05:44 +0530

    Delete Class.py

--- Class.py
+++
@@ -1,6 +0,0 @@
-# Author: Ameet.S.Munavalli
-# To add any sentences as comments in your program just select those lines and press Ctrl+/
-print("Welcome to the world of Python. Happy Learning")
-print("Hello World")
-print("Total hours present in a day for you to learn is")
-print(24-12-6, "Hours")
